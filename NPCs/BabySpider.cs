f6c2597bbe7c68a977acf2a63d15f4348b1516bb
TheAshenWolf <tawdevelopment@gmail.com>
2024-01-10 20:37:10 +0100

    Fixed error in BabySpider.cs
    This error could occur when the last player disconnected and led to broken AI of all future Baby Spiders

--- NPCs/BabySpider.cs
+++ NPCs/BabySpider.cs
@@ -46,7 +46,12 @@ namespace FargoEnemyModifiers.NPCs
             bool flag20 = false;
             float num593 = 500f;
 
-            Player target = Main.player[NPC.FindClosestPlayer()];
+            int player = NPC.FindClosestPlayer();
+            if (player == -1)
+            {
+                return; // This scenario can occur when the last player disconnects
+            }
+            Player target = Main.player[player];
 
             float num595 = Vector2.Distance(target.Center, NPC.Center);
             if (((Vector2.Distance(NPC.Center, value14) > num595 && num595 < num593) || !flag20) && Collision.CanHit(NPC.position, NPC.width, NPC.height, target.position, target.width, target.height))
