ad90945c7bb74fcff709c06177a0f4d8eecff954
TheAshenWolf <tawdevelopment@gmail.com>
2024-01-17 21:48:43 +0100

    Reworked swarming modifier

--- Modifiers/Rarity3/Swarming.cs
+++ Modifiers/Rarity3/Swarming.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FargoEnemyModifiers.Utilities;
+﻿using FargoEnemyModifiers.Utilities;
 using Terraria;
 using Terraria.ID;
 
@@ -11,49 +10,35 @@ namespace FargoEnemyModifiers.Modifiers
         public override string Key => "Swarming";
         public override RarityID Rarity => RarityID.Rare;
         public override float HealthMultiplier => 4f;
+        public override float SizeMultiplier => 2f;
 
         protected bool clonesSpawned;
 
         public override bool PreAI(NPC npc)
         {
-            npc.checkDead();
             if (clonesSpawned || Main.netMode == NetmodeID.MultiplayerClient)
                 return true;
 
-            int owner = npc.whoAmI;
+            SpawnClones(npc);
+                return true;
+        }
 
+        private void SpawnClones(NPC npc)
+        {
             for (int i = 0; i < 8; i++)
             {
                 int x = Main.rand.Next((int)npc.position.X - npc.width - 100, (int)npc.position.X + npc.width + 100);
                 int y = Main.rand.Next((int)npc.position.Y - npc.height - 100, (int)npc.position.Y + npc.height + 100);
 
-                int index = NPC.NewNPC(npc.GetSource_FromAI(), x, y, npc.type);
+                int index = NPC.NewNPC(npc.GetSource_FromAI(), x, y, npc.type, npc.whoAmI);
+
 
                 NPC newNPC = Main.npc[index];
                 newNPC.SetDefaults(npc.type);
-                newNPC.realLife = owner;
-                newNPC.GetGlobalNPC<EnemyModifiersGlobalNPC>().firstTick = false;
-                newNPC.GetGlobalNPC<EnemyModifiersGlobalNPC>().Modifiers.Add(new Swarmer());
-                newNPC.realLife = npc.whoAmI;
-
-                //apply same modifiers to all clones
-                foreach (int modifierType in npc.GetGlobalNPC<EnemyModifiersGlobalNPC>().modifierTypes)
-                {
-                    Modifier newMod = (Activator.CreateInstance(EnemyModifiers.Modifiers[modifierType].GetType()) as Modifier);
-
-                    if (newMod.Key == "Swarming")
-                    {
-                        continue;
-                    }
-
-                    newNPC.GetGlobalNPC<EnemyModifiersGlobalNPC>().ApplyModifier(newNPC, modifierType);
-                    newMod.UpdateModifierStats(newNPC);
-                }
+                newNPC.GetGlobalNPC<EnemyModifiersGlobalNPC>().firstTick = false; // disables modifier picking
+                newNPC.netUpdate = true;
             }
-
             clonesSpawned = true;
-
-            return true;
         }
     }
 }
